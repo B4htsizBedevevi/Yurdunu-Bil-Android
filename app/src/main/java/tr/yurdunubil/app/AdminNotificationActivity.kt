a194310241694462657eb2b44e3514bb9c722940
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-11 00:41:49 +0300

    fix: import width for notification icon row

--- app/src/main/java/tr/yurdunubil/app/AdminNotificationActivity.kt
+++ app/src/main/java/tr/yurdunubil/app/AdminNotificationActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
