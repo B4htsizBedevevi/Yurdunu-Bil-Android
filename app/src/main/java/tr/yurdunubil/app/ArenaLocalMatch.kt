cb77d85657fa4fcf20dc4b57c56381577c84ec3c
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-14 20:39:57 +0300

    ui: warm conversational Turkish copy

--- app/src/main/java/tr/yurdunubil/app/ArenaLocalMatch.kt
+++ app/src/main/java/tr/yurdunubil/app/ArenaLocalMatch.kt
@@ -1,6 +1,8 @@
 package tr.yurdunubil.app
 
+
 import androidx.compose.foundation.background
+import tr.yurdunubil.app.YurdunuBilCopy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
